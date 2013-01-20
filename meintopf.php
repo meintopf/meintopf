76cb1b4919f422b4d7c489f5d0e72230437d4e91
Severin Schols <severin@schols.de>
2013-01-20 22:29:49 +0100

    caching implemented. fetching and displaying seems to work. next up: reposts

--- meintopf.php
+++ meintopf.php
@@ -17,6 +17,24 @@ add_action( 'init', 'meintopf_init' );
 function meintopf_activate() {
 	$options = array("feeds" => array());
 	add_option("meintopf_options",$options);
+	$args = array(
+		'public' => false,
+		'exclude_from_search' => false,
+		'publicly_queryable' => false,
+		'show_ui' => false, 
+		'show_in_menu' => false, 
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'book' ),
+		'capability_type' => 'post',
+		'has_archive' => false, 
+		'hierarchical' => false,
+		'menu_position' => null,
+		'map_meta_cap' => false,
+		'query_var' => false,
+		'can_export' => false,
+		'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields'  )
+	);
+	register_post_type( 'meintopf_item', $args );
 }
 
 // Init the plugin each time
@@ -32,29 +50,87 @@ function meintopf_admin_menu_entries() {
 
 // Show the admin menu page
 function meintopf_menu_page() {
-	if( isset($_POST['feedurl']) ) {
-		meintopf_add_feed($_POST['feedurl']);
-	}
-	echo "<div><h2>Add Feed</h2><form action=\"\" method=\"post\"><label>Feed-URL:</label><input type=\"text\" name=\"feedurl\"><input type=\"submit\"></form></div><hr>";
-	$feeds = meintopf_get_feeds();
-	if (count($feeds) == 0) {
-		echo "No feeds yet!";
+	if (isset($_GET['action']) && $_GET['action'] == "fetch") {
+		meintopf_reader_fetch_feeds();
 	} else {
+		if( isset($_POST['feedurl']) ) {
+			meintopf_add_feed($_POST['feedurl']);
+		}
+		echo "<div><h2>Add Feed</h2><form action=\"\" method=\"post\"><label>Feed-URL:</label><input type=\"text\" name=\"feedurl\"><input type=\"submit\"></form></div><hr>";
+		meintopf_reader();
+	}
+}
+
+function meintopf_reader() {
+	$args = array(
+    'posts_per_page'  => 20,
+    'offset'          => 0,
+    'orderby'         => 'post_date',
+    'order'           => 'DESC',
+    'post_type'       => 'meintopf_item',
+    'post_status'     => '%',
+    'suppress_filters' => true );
+	$myposts = get_posts( $args );
+	foreach( $myposts as $post ) {
+		echo "<div class=\"meintopf_reader_item\">";
+		if ($post->post_title)
+			echo "<h3>$post->post_title</h3>";
+		echo "<div class=\"meintopf_reader_content\">$post->post_content</div><a href=\"#\">Repost</a></div>";
+	} 
+}
+
+// Fetch updates
+function meintopf_reader_fetch_feeds() {
+	// get the list of feeds
+	$feeds = meintopf_get_feeds();
+	
+	// Make sure it's not empty.
+	if (count($feeds) <> 0) {
 		$feed = new SimplePie();
 		$feed->set_feed_url($feeds);
-		$feed->set_cache_duration (600);
+		$feed->set_cache_duration (60);
 		$success = $feed->init();
-		$feed->handle_content_type();
+		
+		// We can haz feed items
 		if ($success) {
+			$feed->handle_content_type();
 			foreach($feed->get_items() as $item) {
-				echo "<div><h3>".$item->get_title()."</h3><div>".$item->get_content()."</div><pre>".$item->get_id()."</pre></div><hr>";
+				// Check if we have that post already
+				$args = array(
+					'meta_key'=>'meintopf_item_guid',
+					'meta_value'=> $item->get_id(),
+					'post_type' => 'meintopf_item'
+				);
+				$my_query = null;
+				$my_query = new WP_Query($args);
+				if ( !$my_query->have_posts() ) { // No, we don't. Insert it.
+					// Create the post itself
+					$post = array(
+						'post_type' =>'meintopf_item',
+						'post_title' => $item->get_title(),
+						'post_content' => $item->get_content(),
+						'post_date_gmt' => $item->get_gmdate('Y-m-d H:i:s')
+					);
+					$id = wp_insert_post($post); // Insert the post
+					
+					// the post guid gets its own metadata field, for later existance checking.
+					update_post_meta($id, 'meintopf_item_guid', $item->get_id());
+					
+					// more metadata
+					$meta = array(
+						'permalink' => $item->get_permalink(),
+						'guid' => $item->get_id(),
+					);
+					update_post_meta($id, 'meintopf_item_metadata', $meta);
+				}
+				wp_reset_query();
 			}
 		}
 	}
 }
 
 /* ******************
- *  Manage Feeds
+ *	Manage Feeds
  * ******************/
 
 function meintopf_get_feeds() {
