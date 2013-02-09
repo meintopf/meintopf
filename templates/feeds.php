53688a85b415ab756198eb38c6e8b625d9278279
Felix Arndt <mail@felixarndt.de>
2013-02-10 00:36:58 +0100

    add favicons to feedlist

--- templates/feeds.php
+++ templates/feeds.php
@@ -3,11 +3,11 @@
 	<h3>Feeds you follow</h3>
 	<table class="wp-list-table widefat fixed">
 		<thead>
-			<tr><th>Feed URL</th><th>Actions</th></tr>
+			<tr><th width="50">Avatar</th><th>Feed URL</th><th>Actions</th></tr>
 		</thead>
 		<tbody>
 			<?php foreach ($this->feeds as $feed) { ?>
-				<tr><td><?= $feed["text"]; ?></td><td><a href="<?= $feed["removal_link"]; ?>">Unfollow</a></td></tr>
+				<tr><td><img src="http://g.etfv.co/<?= $feed["text"]; ?>"></td><td><?= $feed["text"]; ?></td><td><a href="<?= $feed["removal_link"]; ?>">Unfollow</a></td></tr>
 			<?php } ?>
 		</tbody>
 	</table>
