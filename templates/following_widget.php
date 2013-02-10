17698b771acc1183f39dcf9f7d2af42a7d873c24
Felix Arndt <mail@felixarndt.de>
2013-02-10 14:47:14 +0100

    set width and size of favicons

--- templates/following_widget.php
+++ templates/following_widget.php
@@ -1,3 +1,3 @@
 <?php	foreach ($this->feeds as $feed) { ?>
-	<a href="<?= $feed["link"]; ?>" title="<?= $feed["title"]; ?>"><img src="http://g.etfv.co/<?= $feed["link"]; ?>" alt="<?= $feed["title"]; ?>"></a>
+	<a href="<?= $feed["link"]; ?>" title="<?= $feed["title"]; ?>"><img src="http://g.etfv.co/<?= $feed["link"]; ?>" alt="<?= $feed["title"]; ?>" width="16" height="16"></a>
 <?php } ?>
