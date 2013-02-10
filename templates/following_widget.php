34e88964eccc2fb2b3be3932dbe637b562cabc7f
Severin Schols <severin@schols.de>
2013-02-10 14:26:51 +0100

    blogname as tool tip for following widget

--- templates/following_widget.php
+++ templates/following_widget.php
@@ -1,3 +1,3 @@
 <?php	foreach ($this->feeds as $feed) { ?>
-	<a href="<?= $feed["link"]; ?>"><img src="http://g.etfv.co/<?= $feed["link"]; ?>"></a>
+	<a href="<?= $feed["link"]; ?>" title="<?= $feed["title"]; ?>"><img src="http://g.etfv.co/<?= $feed["link"]; ?>" alt="<?= $feed["title"]; ?>"></a>
 <?php } ?>
